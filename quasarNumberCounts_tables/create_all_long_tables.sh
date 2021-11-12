65b988947fa16e0cb4c230ab03405628a7d0c65e
Lynne Jones <ljones.uw@gmail.com>
2021-11-11 19:38:36 -0800

    Add files to create NQSO tables.

---
+++ quasarNumberCounts_tables/create_all_long_tables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python Long_Table.py Shen20 A u
+python Long_Table.py Shen20 A g
+python Long_Table.py Shen20 A r
+python Long_Table.py Shen20 A i
+python Long_Table.py Shen20 A z
+python Long_Table.py Shen20 A y
